cd901ff3d593bcd88222fd3c7be52c6b9775a4cc
geethatulasi <geethatulasi007@gmail.com>
2020-12-30 15:24:54 +0530

    added css and devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_073007) do
+ActiveRecord::Schema.define(version: 2020_12_30_085149) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
